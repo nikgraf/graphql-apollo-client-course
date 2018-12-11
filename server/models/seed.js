02c46a75807e3a81c921d164a12915161f5eba8a
Nik Graf <nik@nikgraf.com>
2018-12-11 18:55:41 +0100

    fixed server seeding

--- server/models/seed.js
+++ server/models/seed.js
@@ -2,48 +2,44 @@
  * npm run seed
  * to seed the database with some initial data
  */
-const { recipeDb, ingredientDb } = require('./db');
+const { recipeDb } = require("./db");
 
 Promise.all([
-  ingredientDb.insert([
-    {
-      _id: 'asdf123',
-      name: 'Salad'
-    },
-  ]),
   recipeDb.insert([
     {
-      _id: '18jlXSrjsoDjJbNqmq8dFoN8UUf',
-      title: 'Wiener schnitzel',
+      _id: "18jlXSrjsoDjJbNqmq8dFoN8UUf",
+      title: "Wiener schnitzel",
       vegetarian: false,
       ingredients: [],
       preparation: []
     },
     {
-      _id: '18jldvbbrVgOtda5kru1iRwqkhd',
-      title: 'Guacamole',
+      _id: "18jldvbbrVgOtda5kru1iRwqkhd",
+      title: "Guacamole",
       vegetarian: true,
       ingredients: [],
       preparation: []
     },
     {
-      _id: '18jlgQ0YCehomI6cBM53uHdWuMM',
-      title: 'Caesar salad',
+      _id: "18jlgQ0YCehomI6cBM53uHdWuMM",
+      title: "Caesar salad",
       vegetarian: false,
       ingredients: [],
       preparation: []
     },
     {
-      _id: '18jliUAUIroOoTa5An3FjeoFM9S',
-      title: 'Apple strudel',
+      _id: "18jliUAUIroOoTa5An3FjeoFM9S",
+      title: "Apple strudel",
       vegetarian: true,
       ingredients: [],
       preparation: []
     }
   ])
-]).then(() => {
-  console.log('✅ Database seeding successful.');
-}).catch((err) => {
-  console.log('⚠️ Database seeding error ⚠️');
-  console.log(err);
-})
+])
+  .then(() => {
+    console.log("✅ Database seeding successful.");
+  })
+  .catch(err => {
+    console.log("⚠️ Database seeding error ⚠️");
+    console.log(err);
+  });
